74f5ca9531e11818ba95940514778b29fc58c134
Alexander <121654315+EgoEano@users.noreply.github.com>
2025-06-05 12:54:14 +0500

    Update RegularExpressions.js

--- library/HTML5+CSS+JS/JS/RegularExpressions.js
+++ library/HTML5+CSS+JS/JS/RegularExpressions.js
@@ -3,3 +3,5 @@ const phoneRegEx = /^\+\d{1,4}[-\s.]*\(?\d{3}\)?[-.\s]*\d{3}[-.\s]*\d{2}[-.\s]*\
 const digitRegEx = /^\d+$/;
 const lettersRegEx = /^\p{L}+$/u;
 const symbolsRegEx = /^[+!@#$%^&*(),.?":{}|<>-]+$/;
+const passwordRegEx = /^[A-Za-z0-9!@#$%^&*()_\-+=\[\]{};:,.<>?/|~]{8,128}$/;
+const passwordRegEx_withRules = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*()_\-+=\[\]{};:,.<>?/|~])[A-Za-z0-9!@#$%^&*()_\-+=\[\]{};:,.<>?/|~]{8,128}$/;
