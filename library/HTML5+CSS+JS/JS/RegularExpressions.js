93b34e3b3ba75bc0d3b47f5a4ce312adb20cb148
Alexander <121654315+EgoEano@users.noreply.github.com>
2025-06-24 15:38:36 +0500

    Update RegularExpressions.js

--- library/HTML5+CSS+JS/JS/RegularExpressions.js
+++ library/HTML5+CSS+JS/JS/RegularExpressions.js
@@ -1,4 +1,5 @@
 const emailRegEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+const emailRegEx_Standart = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
 const phoneRegEx = /^\+\d{1,4}[-\s.]*\(?\d{3}\)?[-.\s]*\d{3}[-.\s]*\d{2}[-.\s]*\d{2}$/;
 const digitRegEx = /^\d+$/;
 const lettersRegEx = /^\p{L}+$/u;
